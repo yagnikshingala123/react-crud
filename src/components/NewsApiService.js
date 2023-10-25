faf3150df42af7b6bc146a14925880dffdac4b76
Yagnik Shingala <yagnikshingala888@gmail.com>
2023-10-25 18:17:41 +0530

    change endpoint

--- src/components/NewsApiService.js
+++ src/components/NewsApiService.js
@@ -4,7 +4,7 @@ const API_KEY = "2888ef306ba84b86982cfac10e0380c3";
 
 const NewsApiService = {
   getData: () => {
-    return axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    return axios.get(`https://newsapi.org/v2/top-headlines?category=general&country=in&apiKey=${API_KEY}`);
   },
 };
 
